9108e427e3345223bf08d4bbe512617b12901814
Abdur Rahman <SaifHomeFolder@Abdurs-MacBook-Air.local>
2016-12-01 18:10:47 -0800

    added some cool jquery, made a lil tot do list for the appfolio guys, started working on the router but thats not quite working yet,

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,41 @@
 /* User experience animations. Dependencies include the jQuery library, Waypoints plugin and Visibility plugin. */
 $(document).ready(function() {
     $('.background').css('height', $(window).height() + 'px');
+
     enableResize();
+    fadeInIntro()
+    $('#key-0').css('display', '');
+    $('#key-1').css('display', '');
+    $('#key-2').css('display', '');
 });
 
 function enableResize() {
     $(window).on('resize', function(event) {
         $('.background').css('height', $(window).height() + 'px');
+
     });
+}
+
+function fadeInIntro() {
+
+    $('#firstImpression').css('display', 'none');
+    $('#big').css('display', 'none');
+    $('#small').css('display', 'none');
+    $('#quote').css('display', 'none');
+
+
+
+    setTimeout(function() {
+        $('#firstImpression').fadeIn(500);
+    }, 500);
+    setTimeout(function() {
+        $('#big').fadeIn(500);
+    }, 1000);
+    setTimeout(function() {
+        $('#small').fadeIn(750);
+    }, 1500);
+    setTimeout(function() {
+        $('#quote').fadeIn(1000);
+    }, 2250);
+
 }
