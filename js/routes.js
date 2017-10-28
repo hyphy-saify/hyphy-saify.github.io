29279fcf9eb6a955496d1ac313715546c39c7a34
lifeofsaif <saif945383@gmail.com>
2017-10-27 18:29:35 -0700

    seeing what happens with cross origin stuff when hosting

--- js/routes.js
+++ js/routes.js
@@ -1,16 +1,5 @@
 var app = angular.module('MainApp', ["ngRoute"])
 
-app.config(function($routeProvider) {
-    $routeProvider.when("/", {
-        templateUrl : "views/about.htm"
-    }).when("/about", {
-        templateUrl : "views/about.htm"
-    }).when("/blog", {
-        templateUrl : "views/blog.htm"
-    }).when("/contact", {
-        templateUrl : "views/contact.htm"
-    })
-});
 
 
 app.controller('MainController', ['$scope', function($scope){
