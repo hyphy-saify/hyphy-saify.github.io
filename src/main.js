6d58daa493cbea966e9327c111a53db27d4cddff
Abdur Rahman <arahman@sonicwall.com>
2019-03-30 16:55:58 -0700

    removing firebase

--- src/main.js
+++ src/main.js
@@ -4,9 +4,6 @@ import Vuex from 'vuex';
 import App from './App.vue';
 import MainPage from './MainPage.vue';
 import OtherPage from './OtherPage.vue';
-import LoginPage from './LoginPage.vue';
-import RegisterPage from './RegisterPage.vue';
-import firebase from 'firebase';
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
 import VueGoogleCharts from 'vue-google-charts'
@@ -16,14 +13,6 @@ console.log(new Date());
 
 // TODO: Replace the following with your own keys from firebase.google.com
 // TODO: Use nconf api keys file
-firebase.initializeApp({
-  apiKey: "AIzaSyAL0LgeNpA6PpQ2MQLX6Nu7wF0w_eX6arY",
-  authDomain: "sign-818b7.firebaseapp.com",
-  databaseURL: "https://sign-818b7.firebaseio.com",
-  projectId: "sign-818b7",
-  storageBucket: "sign-818b7.appspot.com",
-  messagingSenderId: "355449847645"
-});
 
 Vue.use(VueRouter);
 Vue.use(Vuex);
@@ -46,26 +35,11 @@ new Vue({
   }),
   store: new Vuex.Store({
     state: {
-      user: null,
     },
     mutations: {
-      setUser: function (state, _user) {
-        state.user = _user;
-      }
     },
   }),
   created: function () {
-    console.log('Created...');
-    console.log(firebase.auth().currentUser);
-    var vm = this;
-    firebase.auth().onAuthStateChanged(function (_user) {
-      vm.$store.commit('setUser', _user);
-      if (_user) {
-        console.log('Firebase user: [' + _user.email + ']');
-      } else {
-        console.log('Firebase user: [' + 'Logged out' + ']');
-      }
-    });
   },
 });
 
