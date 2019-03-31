0561d2cce831f52672f762d79b997a2f9b1d1008
Abdur Rahman <arahman@sonicwall.com>
2019-03-30 17:43:11 -0700

    changing color for links

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,16 @@
       <router-link to="/blog">Blog</router-link>
     </div>
     <router-view></router-view>
-    <small>Find the Code for this site <a href="https://github.com/lifeofsaif/lifeofsaif.github.io" target="_blank">here</a></small>
+    <small>Find the Code for this site <a href="https://github.com/lifeofsaif/lifeofsaif.github.io" target="_blank">*here*</a></small>
   </div>
 </template>
 
+<style lang="sass">
 
+a {
+  color: #515a6e !important;
+  &:hover {
+    color: #69E781 !important;
+  }
+}
+</style>
