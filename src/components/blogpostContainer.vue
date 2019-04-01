fc5b906d8d158f56965470bcb4013e8b0bb32e52
Abdur Rahman <arahman@sonicwall.com>
2019-04-01 15:17:38 -0700

    adding gradient fade to blogpost container

--- src/components/blogpostContainer.vue
+++ src/components/blogpostContainer.vue
@@ -1,6 +1,6 @@
 <template>
-  <div style="  border-bottom: solid 1px #fff6bc;padding-bottom: 20px; max-width: 70%;">
-    <div style="height: 200px;overflow: hidden;text-overflow: ellipsis;">
+  <div class="blogpost-container" style="  border-bottom: solid 1px #fff6bc;padding-bottom: 20px; max-width: 70%;">
+    <div class="blogpost-container__content" style="height: 200px;overflow: hidden;text-overflow: ellipsis;">
       <slot/>
     </div>
     <small><a :href="continueReading">... continue reading ...</a></small>
@@ -8,6 +8,12 @@
 </template>
 
 <style lang="sass">
+.blogpost-container {
+  &__content {
+  -webkit-mask-image: -webkit-gradient(linear, left top, 
+    left bottom, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)));
+  }
+}
 </style>
 <script>
 export default {
